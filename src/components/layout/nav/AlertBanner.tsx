207fe6ef2ac7965d269f610e952919a1453fb34f
christhomp13 <theinfinitehumanblueprint@gmail.com>
2025-06-14 00:46:08 -0400

    fine tuned alert

--- src/components/layout/nav/AlertBanner.tsx
+++ src/components/layout/nav/AlertBanner.tsx
@@ -18,13 +18,13 @@ const AlertBanner: React.FC<AlertBannerProps> = (props) => {
     <div
       className={`relative bg-gradient-to-r from-[var(--color-secondary)] to-[var(--color-primary)] py-2 px-4 text-white transition-all duration-300 shadow-sm`}
     >
-      <div className="flex sm:items-center sm:justify-center gap-5 max-w-screen-xl mx-auto px-2 relative">
-        <span className="font-medium text-bg-contrast sm:text-center sm:mb-0 text-sm max-w-1/2 sm:max-w-full">
+      <div className="flex sm:items-center sm:justify-center gap-3 max-w-screen-xl mx-auto px-2 relative">
+        <span className="font-medium text-bg-contrast sm:text-center sm:mb-0 text-sm sm:max-w-full">
           {alertBanner?.alertLabel}
         </span>
         <Link
           href={alertBanner?.alertLink ?? ""}
-          className="bg-bg-contrast hover:bg-bg-contrast-light text-bg hover:text-shadow-bg-contrast font-medium py-0.5 sm:py-1 px-4 rounded-md transition duration-200 text-sm flex items-center gap-2 whitespace-nowrap shadow-sm"
+          className="bg-bg-contrast hover:bg-bg-contrast-light text-bg hover:text-shadow-bg-contrast font-medium sm:py-1 px-3 rounded-md transition duration-200 text-sm flex items-center gap-3 whitespace-nowrap shadow-sm mr-2"
         >
           <div>{alertBanner?.alertLinkText}</div>
           <MoveRight className="h-4 w-4" />
@@ -32,7 +32,7 @@ const AlertBanner: React.FC<AlertBannerProps> = (props) => {
       </div>
       <button
         onClick={() => setIsVisible(false)}
-        className="absolute top-1/2 right-2 -translate-y-1/2 text-bg-contrast hover:text-gray-200 cursor-pointer p-1.5"
+        className="absolute top-1/2 right-0 -translate-y-1/2 text-bg-contrast hover:text-gray-200 cursor-pointer p-1"
         aria-label="Close alert"
       >
         <X className="h-5 w-5" />
